2c6cc3c4bfbdf13cf11ed99bfeea94e84e35f795
aurorax-neo <15047150695@163.com>
2025-12-03 12:16:57 +0800

    feat: Add new dependencies and implement basic tests for Kcp2KClient and Kcp2KServer

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,59 @@ pub mod kcp2k_connection;
 pub mod kcp2k_server;
 
 pub use revel_cell;
+
+#[cfg(test)]
+mod tests {
+    use crate::kcp2k_client::Kcp2KClient;
+    use crate::kcp2k_common::{Callback, CallbackType, Kcp2KChannel};
+    use crate::kcp2k_config::Kcp2KConfig;
+    use crate::kcp2k_connection::Kcp2kConnection;
+    use crate::kcp2k_server::Kcp2KServer;
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    pub fn call_back(conn: &Kcp2kConnection, cb: Callback) {
+        println!("{}", cb);
+
+        let now = SystemTime::now();
+        let seconds_since_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis(); // as_secs() 返回 u64
+        // 2. 将 u64 时间戳转换为小端字节序的字节数组
+        let time = seconds_since_epoch.to_le_bytes();
+
+        match cb.r#type {
+            CallbackType::OnConnected => {
+                let _ = conn.send_data(time.as_slice(), Kcp2KChannel::Unreliable);
+            }
+            CallbackType::OnData => {
+                let _ = conn.send_data(time.as_slice(), Kcp2KChannel::Reliable);
+            }
+            CallbackType::OnError => {}
+            CallbackType::OnDisconnected => {}
+        }
+    }
+
+    #[test]
+    pub fn test_server() {
+        env_logger::init();
+        let config = Kcp2KConfig::default();
+
+        let server = Kcp2KServer::new("0.0.0.0:3100".to_string(), config, call_back);
+
+        loop {
+            server.tick();
+        }
+    }
+
+    #[test]
+    pub fn test_client() {
+        env_logger::init();
+        let config = Kcp2KConfig::default();
+
+        let client = Kcp2KClient::new(config, call_back);
+
+        client.connect("10.119.120.22:3100".to_string());
+
+        loop {
+            client.tick();
+        }
+    }
+}
